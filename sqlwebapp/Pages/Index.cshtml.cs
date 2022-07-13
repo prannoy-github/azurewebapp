ec639ceb61c734edaa3df9432e5c5eaf7eda2100
prannoy raj <pranay8889@gmail.com>
2022-07-14 00:12:02 +0200

    Add project files.

---
+++ sqlwebapp/Pages/Index.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using sqlwebapp.models;
+using sqlwebapp.Services;
+
+namespace sqlwebapp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public List<Product> Products;
+
+        public void OnGet()
+        {
+            ProductService productservice = new ProductService();
+            Products = productservice.GetProducts();
+        }
+    }
+}
---
+++ sqlwebapp/models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace sqlwebapp.models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
